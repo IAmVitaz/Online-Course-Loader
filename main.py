cfc16aaf2b90dcece17a6b76bc35809d35017e19
vit_az <vit.azarov@gmail.com>
2022-10-07 21:33:24 -0300

    - Code reformatted to be called from functions. Some refactoring.

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-# import urllib.request
-
-# url_link = "https://filestorage.1iu.ru/api/file/f423dc30-951e-479b-a41c-381f547e7766/normalize/get?download=no&public=no&default=original"
-# file_name = "Тема1. Вводная часть.mp4"
-
-# urllib.request.urlretrieve(url_link, file_name) 
-
 from lib2to3.pgen2 import driver
 from const import BASE_LINK, FIRST_LINK
 from credentials import LOGIN, PASSWORD
@@ -14,13 +7,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
+from webdriver_manager.drivers.chrome import ChromeDriver
 import urllib.request
 
-
-import time
-
-def signIn():
+def signInAntitreningi(driver: ChromeDriver, url: str):
+    driver.get(url)
     wait = WebDriverWait(driver, 10)
     wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@type='email']"))).send_keys(LOGIN)
     wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@type='password']"))).send_keys(PASSWORD)
@@ -28,36 +19,25 @@ def signIn():
 
     wait.until(EC.visibility_of_element_located((By.XPATH, "//div[@class='block noflex courseinfoblock']")))
 
-def getLessonLinks() -> list:
+def getLessonLinks(driver: ChromeDriver, baseUrl: str) -> list:
     soup = BeautifulSoup(driver.page_source, "html.parser")
     menu = soup.find_all("div", class_="rmenu")
     menuItems = menu[1].find_all(class_=["rmenu__item", "right-icon"])
 
     linksArray = []
     for item in menuItems:
-        linksArray.append(item['href'])
+        linksArray.append(baseUrl + item['href'])
     return linksArray
 
-# Sign in
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get(BASE_LINK + FIRST_LINK)
+def saveListOfURLsToFile(list: list, baseURL: str, fileName: str):
+    with open(fileName, "w") as file:
+        for item in list:
+            file.write(item + "\n")
 
-signIn()
-
-# Retrieve lessons links
-linksArray = getLessonLinks()
-
-
-with open("lesson_links.txt", "w") as file:
-    for line in linksArray:
-        file.write(BASE_LINK + line + "\n")
-
-
-# Open each link and search for video
-for link in linksArray:
+def downloadVideosIfExists(driver: ChromeDriver, lesson: str):
     try:
-        print("Trying to open link" + link)
-        driver.get(BASE_LINK + link)
+        print("Trying to open link" + lesson)
+        driver.get(lesson)
         wait = WebDriverWait(driver, 10)
         wait.until(EC.visibility_of_element_located((By.XPATH, "//div[@class='lesson_name_container']")))
         soup = BeautifulSoup(driver.page_source, "html.parser")
@@ -74,12 +54,24 @@ for link in linksArray:
             soup = BeautifulSoup(driver.page_source, "html.parser")
             targetVideoUrl = soup.find("source")['src']
             urllib.request.urlretrieve(url = targetVideoUrl, filename = lessonName) 
-            print("Link " + link + " have been saved successfully") 
+            print("Link " + lesson + " have been saved successfully") 
         else:
-            print("Video file " + link + " does not exist in the lesson") 
-
-
+            print("Video file " + lesson + " does not exist in the lesson") 
     except:
-        print("Something got wrong with " + link)
+        print("Something got wrong with " + lesson)
+
+# Sign in
+driver = webdriver.Chrome(ChromeDriverManager().install())
+startingPointURL = BASE_LINK + FIRST_LINK
+signInAntitreningi(driver = driver, url = startingPointURL)
+
+# Retrieve lessons links
+linksArray = getLessonLinks(driver = driver, baseUrl = BASE_LINK)
+
+saveListOfURLsToFile(list = linksArray, baseURL = BASE_LINK, fileName = "lesson_links.txt")
+
+link = linksArray[0]
+# Open each link and search for video
+downloadVideosIfExists(driver = driver, lesson = link)
 
 driver.close()
